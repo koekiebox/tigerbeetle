15bc0cee83738cac7f4040ba1e9dba192dcc4da0
Isaac Freund <isaac.freund@coil.com>
2022-03-22 13:16:41 +0100

    LSM: Tweak SetAssociativeCache API, implement eviction
    The new get_or_put() API makes it possible to avoid a copy on insertion
    if the key already exists.

--- src/lsm/set_associative_cache.zig
+++ src/lsm/set_associative_cache.zig
@@ -157,31 +157,52 @@ pub fn SetAssociativeCache(
             return self.search(set, key);
         }
 
-        pub fn put(self: *Self, value: Value) void {
-            const key = key_from_value(value);
+        pub const GetOrPutResult = struct {
+            value_ptr: *Value,
+            found_existing: bool,
+        };
+
+        pub fn get_or_put(self: *Self, key: Key) GetOrPutResult {
             const set = self.associate(key);
 
-            if (self.search(set, key)) |exists| {
-                exists.* = value;
-                return;
+            if (self.search(set, key)) |existing| {
+                return .{
+                    .value_ptr = existing,
+                    .found_existing = true,
+                };
             }
 
-            // TODO Find free way.
-            // TODO Evict if necessary.
-            const way = 0;
+            var way = Clocks.get(self.clocks, set.index);
+            while (true) : (way +%= 1) {
+                const count = Counts.get(self.counts, set.offset + way);
+
+                // Free way found
+                if (count == 0) break;
+
+                Counts.set(self.counts, set.offset + way, count - 1);
+            }
+            Clocks.set(self.clocks, set.index, way +% 1);
 
             set.tags[way] = set.tag;
-            set.values[way] = value;
 
             Counts.set(self.counts, set.offset + way, 1);
+
+            return .{
+                .value_ptr = &set.values[way],
+                .found_existing = false,
+            };
         }
 
         fn search(self: *Self, set: Set, key: Key) ?*Value {
             for (set.tags) |tag, way| {
                 // TODO self.counts.get()
-                if (tag == set.tag and Counts.get(self.counts, set.offset + way) > 0) {
-                    var value_ptr = &set.values[way];
-                    if (equal(key_from_value(value_ptr.*), key)) return value_ptr;
+                const count = Counts.get(self.counts, set.offset + way);
+                if (tag == set.tag and count > 0) {
+                    const value_ptr = &set.values[way];
+                    if (equal(key_from_value(value_ptr.*), key)) {
+                        Counts.set(self.counts, set.offset + way, count +| 1);
+                        return value_ptr;
+                    }
                 }
             }
             return null;
@@ -196,7 +217,7 @@ pub fn SetAssociativeCache(
 
         inline fn associate(self: *Self, key: Key) Set {
             const entropy = hash(key);
-            
+
             const tag = @truncate(Tag, entropy >> math.log2_int(u64, self.sets));
             const index = entropy % self.sets;
             const offset = index * layout.ways;
